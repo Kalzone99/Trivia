2e3b50fba6fa770d5872e28ef0aad35092f6e0de
Kevin Selvais <kalzone99@gmail.com>
2025-03-05 12:51:12 +0100

    bug fixes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // 👈 Make sure these paths exist in your project
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
